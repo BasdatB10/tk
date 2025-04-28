639216542a43edb7e738243b12f5aa8abd59d52b
Sulthan Adrin <sulthanadrin0806@gmail.com>
2025-04-28 17:23:41 +0700

    menyelesaikan frontend fitur adoption

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from base.views import *
+from adoption_management.views import *
 
 app_name = 'base'
 
@@ -8,4 +9,7 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
+    path('manage-adopt/', manage_adopt, name='manage-adopt'),
+    path('adopter-page/', show_adopter_page, name='adopter-page'),
+    path('adopter-list/', show_adopter_list, name='adopter-list'),
 ]
